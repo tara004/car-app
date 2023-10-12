e51b4c8edcbe50dc6b6d225de21e939dab1c5f3e
crossi72 <claudio@eurosofltab.com>
2023-10-12 12:59:39 +0200

    added Auto class

--- prima app/Program.cs
+++ prima app/Program.cs
@@ -1,2 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Reflection.Metadata.Ecma335;
+
+Auto miaAuto;
+
+miaAuto = new Auto();
+
+miaAuto.stampa();
+
+Console.ReadLine();
+
+class Auto {
+	//tipi personalizzati
+	public enum motore {
+		benzina,
+		diesel,
+		elettrico
+	}
+
+	//proprietà
+	public string marca;
+	public string modello;
+	public string colore;
+	public motore tipoMotore;
+
+	//costruttore
+	public Auto(){
+		this.marca = "Fiat";
+		this.modello = "Punto";
+		this.tipoMotore = motore.diesel;
+		this.colore = "Rosso";
+	}
+
+	public void stampa(){
+		//stampo le caratteristiche dell'auto
+		Console.WriteLine("marca: " + this.marca); 
+		Console.WriteLine("modello: " + this.modello); 
+		Console.WriteLine("motore: " + this.tipoMotore); 
+		Console.WriteLine("colore: " + this.colore);
+	}
+}
