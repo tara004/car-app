e1a6564a5f0e63423f6d2141680475e35cc94045
crossi72 <claudio@eurosofltab.com>
2023-10-12 17:07:23 +0200

    Added new methods

--- prima app/Program.cs
+++ prima app/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.SymbolStore;
 using System.Reflection.Metadata.Ecma335;
 
 Auto miaAuto;
@@ -6,11 +7,17 @@ Auto miaAuto;
 miaAuto = new Auto("Fiat", "Multipla", "Argento", Auto.motore.benzina);
 
 miaAuto.StampaDescrizione();
+miaAuto.StampaStato();
+
+miaAuto.accendi();
+
+miaAuto.StampaStato();
 
 Console.ReadLine();
 
 class Auto {
 	//tipi personalizzati
+
 	public enum motore {
 		benzina,
 		diesel,
@@ -18,10 +25,16 @@ class Auto {
 	}
 
 	//proprietà
+	public int livelloCarburante;
+	/// <summary>
+	/// Marca del veicolo
+	/// </summary>
 	public string marca;
 	public string modello;
 	public string colore;
 	public motore tipoMotore;
+
+	private bool accesa;
 
 	//costruttore
 	public Auto(){
@@ -45,10 +58,20 @@ class Auto {
 		this.tipoMotore = tipoMotore;
 	}
 
+	/// <summary>
+	/// Stampa la descrizione del veicolo
+	/// </summary>
 	public void StampaDescrizione()
 	{
 		//stampo le caratteristiche dell'auto
 		Console.WriteLine(GeneraDescrizione());
+	}
+	
+	/// <summary>
+	/// Stampa lo stato attuale del veicolo
+	/// </summary>
+	public void StampaStato() {
+		Console.Write(GeneraStato());
 	}
 
 	/// <summary>
@@ -68,5 +91,47 @@ class Auto {
 
 		//restituisco il risultato
 		return result;
+	}
+
+	/// <summary>
+	/// Genera lo stato (carburante, accensione, ...) del veicolo
+	/// </summary>
+	/// <returns>Lo stato del veicolo</returns>
+	public string GeneraStato(){
+		string result;
+
+		result = $"carburante: {this.livelloCarburante} \n";
+		if (this.accesa){
+			//se il veicolo è acceso, scrivo "accesa"
+			result += "accesa\n";
+		}else {
+			//altrimenti scrivo "spenta"
+			result += "spenta\n";
+		}
+	
+		return result;
+	}
+
+	/// <summary>
+	/// Tenta di avviare il veicolo <para>
+	/// Se non c'è carburante l'accensione fallisce</para>
+	/// </summary>
+	public void accendi(){
+		//verifico il livello del carburante
+		if (this.livelloCarburante > 0){
+			//se è maggiore di zero accendo il veicolo
+			this.accesa= true;
+		} else {
+			//se è minore o uguale a zero spengo il veicolo
+			this.accesa = false;
+		}
+	}
+
+	/// <summary>
+	/// Spegne il veicolo
+	/// </summary>
+	public void spegni(){
+		//spegne il veicolo
+		this.accesa = false;
 	}
 }
