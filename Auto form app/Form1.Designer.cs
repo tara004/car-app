4cc42c6c82dd58a3ceae7da4f41ebb72b69626ec
fromani10 <romanifede10@gmail.com>
2023-10-19 15:14:31 +0200

    c

--- Auto form app/Form1.Designer.cs
+++ Auto form app/Form1.Designer.cs
@@ -1,96 +1,113 @@
 ﻿namespace Auto_form_app
 {
-	partial class Form1
-	{
-		/// <summary>
-		///  Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-		/// <summary>
-		///  Clean up any resources being used.
-		/// </summary>
-		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.lblAutoStatus = new System.Windows.Forms.Label();
-			this.btnStart = new System.Windows.Forms.Button();
-			this.btnAccelerate = new System.Windows.Forms.Button();
-			this.pbaSpeed = new System.Windows.Forms.ProgressBar();
-			this.SuspendLayout();
-			// 
-			// lblAutoStatus
-			// 
-			this.lblAutoStatus.AutoSize = true;
-			this.lblAutoStatus.Location = new System.Drawing.Point(46, 185);
-			this.lblAutoStatus.Name = "lblAutoStatus";
-			this.lblAutoStatus.Size = new System.Drawing.Size(78, 32);
-			this.lblAutoStatus.TabIndex = 0;
-			this.lblAutoStatus.Text = "label1";
-			// 
-			// btnStart
-			// 
-			this.btnStart.Location = new System.Drawing.Point(46, 48);
-			this.btnStart.Name = "btnStart";
-			this.btnStart.Size = new System.Drawing.Size(157, 86);
-			this.btnStart.TabIndex = 1;
-			this.btnStart.Text = "Start";
-			this.btnStart.UseVisualStyleBackColor = true;
-			this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
-			// 
-			// btnAccelerate
-			// 
-			this.btnAccelerate.Location = new System.Drawing.Point(232, 48);
-			this.btnAccelerate.Name = "btnAccelerate";
-			this.btnAccelerate.Size = new System.Drawing.Size(157, 86);
-			this.btnAccelerate.TabIndex = 2;
-			this.btnAccelerate.Text = "Accelerate";
-			this.btnAccelerate.UseVisualStyleBackColor = true;
-			this.btnAccelerate.Click += new System.EventHandler(this.btnAccelerate_Click);
-			// 
-			// pbaSpeed
-			// 
-			this.pbaSpeed.Location = new System.Drawing.Point(46, 236);
-			this.pbaSpeed.Name = "pbaSpeed";
-			this.pbaSpeed.Size = new System.Drawing.Size(708, 46);
-			this.pbaSpeed.TabIndex = 3;
-			// 
-			// Form1
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.pbaSpeed);
-			this.Controls.Add(this.btnAccelerate);
-			this.Controls.Add(this.btnStart);
-			this.Controls.Add(this.lblAutoStatus);
-			this.Name = "Form1";
-			this.Text = "Form1";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblAutoStatus = new Label();
+            btnStart = new Button();
+            btnAccelerate = new Button();
+            pbaSpeed = new ProgressBar();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // lblAutoStatus
+            // 
+            lblAutoStatus.AutoSize = true;
+            lblAutoStatus.Location = new Point(28, 116);
+            lblAutoStatus.Margin = new Padding(2, 0, 2, 0);
+            lblAutoStatus.Name = "lblAutoStatus";
+            lblAutoStatus.Size = new Size(50, 20);
+            lblAutoStatus.TabIndex = 0;
+            lblAutoStatus.Text = "label1";
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(28, 30);
+            btnStart.Margin = new Padding(2, 2, 2, 2);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(97, 54);
+            btnStart.TabIndex = 1;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // btnAccelerate
+            // 
+            btnAccelerate.Location = new Point(143, 30);
+            btnAccelerate.Margin = new Padding(2, 2, 2, 2);
+            btnAccelerate.Name = "btnAccelerate";
+            btnAccelerate.Size = new Size(97, 54);
+            btnAccelerate.TabIndex = 2;
+            btnAccelerate.Text = "Accelerate";
+            btnAccelerate.UseVisualStyleBackColor = true;
+            btnAccelerate.Click += btnAccelerate_Click;
+            // 
+            // pbaSpeed
+            // 
+            pbaSpeed.Location = new Point(28, 148);
+            pbaSpeed.Margin = new Padding(2, 2, 2, 2);
+            pbaSpeed.Name = "pbaSpeed";
+            pbaSpeed.Size = new Size(436, 29);
+            pbaSpeed.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(338, 30);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 54);
+            button1.TabIndex = 4;
+            button1.Text = "decelera";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(492, 281);
+            Controls.Add(button1);
+            Controls.Add(pbaSpeed);
+            Controls.Add(btnAccelerate);
+            Controls.Add(btnStart);
+            Controls.Add(lblAutoStatus);
+            Margin = new Padding(2, 2, 2, 2);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		}
+        #endregion
 
-		#endregion
-
-		private Label lblAutoStatus;
-		private Button btnStart;
-		private Button btnAccelerate;
-		private ProgressBar pbaSpeed;
-	}
+        private Label lblAutoStatus;
+        private Button btnStart;
+        private Button btnAccelerate;
+        private ProgressBar pbaSpeed;
+        private Button button1;
+    }
 }
